c03c61612858be133aaf865dc580d2323d42b18d
[Anatoly] <[anatoly.akopyan@yandex.ru]>
2022-12-18 15:32:45 +0300

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -5,6 +5,8 @@
 // - объект с адресом доставки: {street, house, entrance, floor, flat}
 // - список товаров в заказе
 // - стоимость заказа с учетом скидок и доставки
+
+
 // Как результат функции требуется вернуть JSON,
 // cформированный в соответствии с правилами:
 // Объект data содержит все данные
@@ -34,20 +36,14 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {order: {address, sum}, client: {name, phone}, goods:[title, count]};
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push(goods[i].title);
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
-}
+    data.order.address = "ул. " + address.street + ", дом " + address.house + " , " + address.entrance + " подъезд," + address.floor + " этаж, " + "кв. " + address.flat;;
+    data.order.sum = sum;
+    data.client = name + " " + phone;
+    let jsonData = JSON.stringify({data});
+    return jsonData
+    }
